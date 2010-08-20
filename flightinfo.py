4d291c6810a9f75e2dc31e792c356257aeab6763
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2010-08-20 22:10:31 +0200

    Add Airline class, and make the FlightParser parse some of the flight data.

--- flightinfo.py
+++ flightinfo.py
@@ -19,20 +19,39 @@ class TestAirPort(unittest.TestCase):
         self.assertEqual(a.code, code)
         self.assertEqual(a.name, name)
 
+class Airline(object):
+    def __init__(self, code, name):
+        self.code = code
+        self.name = name
+
+class TestAirline(unittest.TestCase):
+    def testBasic(self):
+        code = "WF"
+        name =  "Widerøe"
+        a = Airline(code, name)
+        self.assertEqual(a.code, code)
+        self.assertEqual(a.name, name)
+
+
+    
+
 
 class Flight:
-    def __init__(self, unique_id, flight_id):
+    def __init__(self, unique_id, flight_id, airline):
         self.unique_id = unique_id
         self.flight_id = flight_id
+        self.airline = airline
 
 class TestFlight(unittest.TestCase):
     def testBasic(self):
         unique_id = "12345434"
         flight_id = "SK123"
-        f = Flight(unique_id, flight_id)
+        airline = Airline("WF", "Widerøe")
+        f = Flight(unique_id, flight_id, airline)
         
         self.assertEqual(unique_id, f.unique_id)
         self.assertEqual(flight_id, f.flight_id)
+        self.assertEqual(airline, f.airline)
 
 
 class Enum(set):
@@ -93,6 +112,8 @@ class FlightInformationService(object):
         return urllib.urlencode(data)
 
 
+
+
 class TestFlightInformationService(unittest.TestCase):
     def setUp(self):
         airport = AirPort("OSL", "Gardermoen Oslo")
@@ -117,6 +138,44 @@ class TestFlightInformationService(unittest.TestCase):
         v = FlightInformationService.generateQueryString(self.query)
         self.assertEqual(v, "airport=OSL&direction=A")
 
+import xml.etree.ElementTree as ET 
+
+class FlightParser(object):
+    @staticmethod 
+    def parseFlights(xml_file):
+        tree = ET.XML(xml_file)
+
+        flights = []
+
+        for node in tree.getiterator('flight'):
+            unique_id = node.attrib.get("uniqueID")
+            flight_id = node.find("flight_id").text
+            airline = Airline("SK", "SAS")
+            flight = Flight(unique_id, flight_id, airline)
+#            for child in node.getchildren():
+#                if child.text:
+#                    print child.tag + ": " + child.text
+            flights.append(flight)
+
+        return flights
+
+
+class TestFlightParser(unittest.TestCase):
+
+    def testTrondheimData(self):
+        xml_data = open("testdata/trondheimflights.xml").read()
+        
+        flights = FlightParser.parseFlights(xml_data)
+        self.assertEqual(2, len(flights))
+
+        flight1 = flights[0]
+        self.assertEqual("1170765", flight1.unique_id) 
+        self.assertEqual("BGT073", flight1.flight_id)
+
+        flight2 = flights[1]
+        self.assertEqual("1176338", flight2.unique_id)
+        self.assertEqual("SK381", flight2.flight_id)
+
         
 
 #data = {}
@@ -127,13 +186,12 @@ class TestFlightInformationService(unittest.TestCase):
 #print url_values
 
 #url = "http://flydata.avinor.no/XmlFeed.asp"
+
 #full_url = url + '?' + url_values
 
 #response = urllib2.urlopen(full_url)
 
 #xml = response.read()
-
-
 
 
 
