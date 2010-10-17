a17e1c4a6bfcfdba3d347092219f26abc98e77e4
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2010-10-17 21:06:43 +0200

    Take airport on command line, also add possibility to turn off arrivals/departures.

--- examples/flightgetter.py
+++ examples/flightgetter.py
@@ -4,7 +4,7 @@
 import sys
 sys.path.append('../') 
 import urllib2
-
+from optparse import OptionParser
 from flightinfo.airlinefactory import AirlineFactory
 from flightinfo.airlineparser import AirlineParser
 from flightinfo.airport import AirPort
@@ -15,6 +15,17 @@ from flightinfo.flightinformationservice import FlightInformationService
 from flightinfo.flightparser import FlightParser
 from flightinfo.query import Query
 
+parser = OptionParser()
+parser.add_option("--departures", action="store_true", dest="departures_only", 
+                  help="Print information about only departures.", 
+                  default=False)
+parser.add_option("--arrivals", action="store_true", dest="arrivals_only", 
+                  help="Print information about only arrivals.", default=False)
+
+
+(options, args) = parser.parse_args()
+airportname = args[0]
+
 
 airports_xml = FlightInformationService.download_airport_xml()
 airports = AirPortParser.parse_airports(airports_xml)
@@ -24,7 +35,7 @@ airlines_xml = FlightInformationService.download_airline_xml()
 airlines = AirlineParser.parse_airlines(airlines_xml)
 airline_factory = AirlineFactory(airlines)
 
-airport = AirPort("TRD", "Trondheim")
+airport = airport_factory.get_airport_by_code(airportname)
 query = Query(airport)
 xml = FlightInformationService.download_flight_xml(query)
 
@@ -36,12 +47,16 @@ def is_departure(flight):
 def is_arrival(flight):
     return flight.direction == Flight.Directions.ARRIVAL
 
-print "\nDepartures:"
-for f in filter(is_departure, flights):
-    print f
+print "Flight information for %s (%s)" % (airport.name, airport.code)
 
-print "\nArrivals:"
-for f in filter(is_arrival, flights):
-    print f
+if not options.arrivals_only:
+    print "\nDepartures:"
+    for f in filter(is_departure, flights):
+        print f
+
+if not options.departures_only:
+    print "\nArrivals:"
+    for f in filter(is_arrival, flights):
+        print f
 
 
